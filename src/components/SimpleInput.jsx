7a712cb0d1df290a4cfed74941d83595c2d20633
Cory Pollard <astandard87@gmail.com>
2023-01-25 11:24:09 -0600

    added form validation

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,12 +1,60 @@
-const SimpleInput = (props) => {
+import React, { useState, useEffect } from 'react';
+
+const SimpleInput = props => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [enteredNameIsValid]);
+
+  const nameInputChangeHandler = e => {
+    setEnteredNameTouched(true);
+    setEnteredName(e.target.value);
+  };
+
+  const nameInputBlurHandler = () => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = e => {
+    e.preventDefault();
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) return;
+    console.log(enteredName);
+    setEnteredName('');
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = `form-control ${
+    nameInputIsInvalid ? 'invalid' : ''
+  }`;
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
